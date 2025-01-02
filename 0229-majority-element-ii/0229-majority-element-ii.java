9e1ab2761c4cc4dfcbe0f3d7a53f35c7014253eb
mkshashank <84658486+mkshashank@users.noreply.github.com>
2025-01-02 11:00:11 +0530

    Time: 2 ms (99.32%), Space: 48.6 MB (8.79%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,14 +1,45 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> ans = new ArrayList<Integer>();
-        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
-        int min = nums.length/3;
+        int count1 = 0, count2 = 0, ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE, min = nums.length/3;
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(count1 == 0 && nums[i] != ele2)
+            {
+                count1 = 1;
+                ele1 = nums[i];
+            }
+            else if(count2 == 0 && nums[i] != ele1)
+            {
+                count2 = 1;
+                ele2 = nums[i];
+            }
+            else if(ele1 == nums[i])
+                count1++;
+            else if(ele2 == nums[i])
+                count2++;
+            else
+            {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0; 
+
         for(int x : nums)
         {
-            map.put(x,map.getOrDefault(x,0)+1);
-            if(map.get(x) > min && !ans.contains(x))
-                ans.add(x);
+            if(x == ele1)
+                count1++;
+            if(x == ele2)
+                count2++;
         }
+
+        if(count1 > min)
+            ans.add(ele1);
+        if(count2 > min)
+            ans.add(ele2);
 
         return ans;
     }
