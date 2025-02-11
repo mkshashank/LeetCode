a5d3f9299d50006c0364024d267bf95b806967b2
mkshashank <84658486+mkshashank@users.noreply.github.com>
2025-02-11 13:50:29 +0530

    Time: 1 ms (99.46%), Space: 42.1 MB (84.75%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String removeOccurrences(String s, String part) 
+    {
+        StringBuilder sb = new StringBuilder(s);
+        while(sb.indexOf(part) != -1)
+        {
+            sb.delete(sb.indexOf(part),sb.indexOf(part)+part.length());
+        }    
+        return sb.toString();
+    }
+}
