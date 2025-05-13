cf2e64fbb499ff2a74ad913054b3b20fe19945db
mkshashank <84658486+mkshashank@users.noreply.github.com>
2025-05-13 20:12:08 +0530

    Time: 1 ms (85.71%), Space: 40.9 MB (69.16%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -2,14 +2,13 @@ class Solution
 {
     public int mySqrt(int x) 
     {
-        long low = 1, high = x, ans = 0;
+        long low = 1, high = x;
         while(low <= high)
         {
             long mid = low+(high-low)/2;
             long prod = mid*mid;
             if(prod <= x)
             {
-                ans = mid;
                 low = mid+1;
             }
             else
@@ -17,6 +16,6 @@ class Solution
                 high = mid-1;
             }
         }
-        return (int)ans;
+        return (int)high;
     }
 }
