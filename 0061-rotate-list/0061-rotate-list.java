b15d07f5e262d144917d1a998856ad9b8a742547
mkshashank <84658486+mkshashank@users.noreply.github.com>
2025-08-20 21:06:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution 
+{
+    public void reverse(int[] arr, int i, int j)
+    {
+        while(i < j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public ListNode rotateRight(ListNode head, int k) 
+    {
+        if(head == null || head.next == null || k == 0) return head;
+        ListNode temp = head;
+        List<Integer> list = new ArrayList<>();
+
+        while(temp != null)
+        {
+            list.add(temp.val);
+            temp = temp.next;
+        }
+
+        int[] arr = new int[list.size()];
+        for(int i = 0; i < list.size(); i++)
+            arr[i] = list.get(i);
+
+        k = k % arr.length;
+        reverse(arr,0,k);
+        reverse(arr,arr.length-k,arr.length-1);
+        reverse(arr,0,arr.length-1);
+        k = 0;
+        temp = head;
+        while(temp != null)
+        {
+            temp.val = arr[k++];
+            temp = temp.next;
+        }
+        return head;
+    }
+}
