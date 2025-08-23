e10361cf312b67120124158fcacd16a60172e94b
mkshashank <84658486+mkshashank@users.noreply.github.com>
2025-08-23 08:57:21 +0530

    Time: 27 ms (54.55%), Space: 57.4 MB (12.69%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,46 +1,35 @@
-class Solution {
+class Solution 
+{
+    public void mergeSort(int[] arr, int low, int high)
+    {
+        if(low >= high) return;
+        int mid = low + (high-low)/2;
+        mergeSort(arr,low,mid);
+        mergeSort(arr,mid+1,high);
+        merge(arr,low,mid,high);
+    }
+
     public void merge(int[] arr, int low, int mid, int high)
     {
-        int i = low;
-        int j = mid+1;
-        int k = 0;
         int[] temp = new int[high-low+1];
+        int ind = 0, i = low, j = mid+1;
         while(i <= mid && j <= high)
         {
             if(arr[i] <= arr[j])
-                temp[k++] = arr[i++];
+                temp[ind++] = arr[i++];
             else
-                temp[k++] = arr[j++];
+                temp[ind++] = arr[j++];
         }
-        while(i <= mid)
-            temp[k++] = arr[i++];
-        while(j <= high)
-            temp[k++] = arr[j++];
-        for(int x = 0; x < temp.length; x++)
-        {
-            arr[low+x] = temp[x];
-        }
+        while(i <= mid) temp[ind++] = arr[i++];
+        while(j <= high) temp[ind++] = arr[j++];
+        
+        for(i = low; i <= high; i++)
+            arr[i] = temp[i-low];
     }
 
-    public void mergeSort(int[] arr, int low, int high)
+    public int[] sortArray(int[] nums) 
     {
-        if(low < high)
-        {
-            int mid = (low+high)/2;
-            mergeSort(arr,low,mid);
-            mergeSort(arr,mid+1,high);
-            merge(arr,low,mid,high);
-        }
-    }
-
-    public void mS(int[] nums)
-    {
-        int low = 0;
-        int high = nums.length-1;
-        mergeSort(nums,low,high);
-    }
-    public int[] sortArray(int[] nums) {
-        mS(nums);
+        mergeSort(nums,0,nums.length-1);
         return nums;
     }
 }
