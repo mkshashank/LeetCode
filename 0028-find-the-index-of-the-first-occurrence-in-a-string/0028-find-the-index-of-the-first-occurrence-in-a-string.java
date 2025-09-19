0178112d90181555a75cc1696d65d98c3f8511c2
mkshashank <84658486+mkshashank@users.noreply.github.com>
2025-09-19 22:17:17 +0530

    Time: 0 ms (100%), Space: 41.7 MB (50.35%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -2,17 +2,14 @@ class Solution
 {
     public int strStr(String haystack, String needle) 
     {
-        if(needle == null || needle.isEmpty()) return -1;
-        char c = needle.charAt(0);
         for(int i = 0; i <= haystack.length()-needle.length(); i++)
         {
-            if(haystack.charAt(i) == c)
+            if(needle.charAt(0) == haystack.charAt(i))
             {
-                String s = haystack.substring(i,i+needle.length());
-                if(s.equals(needle))
+                if(haystack.substring(i,i+needle.length()).equals(needle))
                     return i;
             }
-        }    
+        }
         return -1;
     }
 }
