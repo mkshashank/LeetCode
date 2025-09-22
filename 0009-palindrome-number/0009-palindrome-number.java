fcad175ecc1dcf217a3d3018799acaa53ef2d260
mkshashank <84658486+mkshashank@users.noreply.github.com>
2025-09-22 22:34:43 +0530

    Time: 5 ms (65.43%), Space: 43.9 MB (76.5%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,13 +1,15 @@
-class Solution {
-    public boolean isPalindrome(int x) {
+class Solution 
+{
+    public boolean isPalindrome(int x) 
+    {
         if(x < 0) return false;
-        int ans = 0, temp = x;
+        int res = 0, temp = x;
         while(temp > 0)
         {
             int lastDigit = temp % 10;
-            ans = ans*10 + lastDigit;
+            res = res * 10 + lastDigit;
             temp /= 10;
         }
-        return ans == x;
+        return res == x;
     }
 }
