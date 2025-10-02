1a2693fb26485f44814d1d3a9a60bdfdc6cc53a5
mkshashank <84658486+mkshashank@users.noreply.github.com>
2025-10-02 15:39:26 +0530

    Time: 1 ms (30.32%), Space: 41.1 MB (45.81%) - LeetHub

---
+++ 3100-water-bottles-ii/3100-water-bottles-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int total=numBottles;
+        while(numBottles>=numExchange){
+            total++;
+            numBottles-=numExchange;
+            numBottles++;
+            numExchange++;
+        }
+        return total;
+    }
+}
