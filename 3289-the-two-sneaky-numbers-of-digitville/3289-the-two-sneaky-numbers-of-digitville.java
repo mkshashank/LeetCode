4bc9ea06fda2c975ff234a24e9f5ad8ed51df99a
mkshashank <84658486+mkshashank@users.noreply.github.com>
2025-10-31 12:16:37 +0530

    Time: 1 ms (100%), Space: 44.6 MB (77.02%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
@@ -1,15 +1,24 @@
-class Solution {
-    public int[] getSneakyNumbers(int[] nums) {
+class Solution 
+{
+    public int[] getSneakyNumbers(int[] nums) 
+    {
         int[] ans = new int[2];
-        int pos = 0;
-        for(int i = 0; i < nums.length; i++)
+        int[] hashArr = new int[nums.length];
+
+        for(int x : nums)
+            hashArr[x]++;
+
+        int i = 0;
+        for(int j = 0; j < hashArr.length; j++)
         {
-            for(int j = i+1; j < nums.length; j++)
+            if(hashArr[j] == 2)
             {
-                if(nums[i] == nums[j])
-                    ans[pos++] = nums[i];
+                ans[i] = j;
+                i++;
             }
+            if(i == 2) break;
         }
+
         return ans;
     }
 }
