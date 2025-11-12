ddb054048ff72154e8d690ef101f72d9f5716fc8
mkshashank <84658486+mkshashank@users.noreply.github.com>
2025-11-12 19:43:17 +0530

    Time: 13 ms (98.56%), Space: 141.4 MB (10.19%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.java
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.java
@@ -1,39 +1,23 @@
 class Solution 
 {
-    public boolean search(int[] nums, int num)
-    {
-        int low = 0, high = nums.length-1;
-        while(low <= high)
-        {
-            int mid = (low+high)/2;
-            if(nums[mid] == num)
-                return true;
-            else if(nums[mid] < num)
-                low = mid+1;
-            else high = mid-1;
-        }
-        return false;
-    }
-
-    public boolean isLonely(int[] nums, int num)
-    {
-        int prev = num-1, next = num+1;
-        return !search(nums,prev) && !search(nums,next);
-    }
-
     public List<Integer> findLonely(int[] nums) 
     {
         List<Integer> ans = new ArrayList<>();
-        Map<Integer,Integer> map = new HashMap<>();
-        Arrays.sort(nums);
-
+        int maxi = 0;
         for(int x : nums)
-            map.put(x,map.getOrDefault(x,0)+1);
+            maxi = Math.max(maxi,x);
+
+        int[] freq = new int[maxi+2];
 
         for(int x : nums)
         {
-            if(isLonely(nums,x) && (map.get(x) == 1))
-                ans.add(x);
+            freq[x]++;
+        }    
+
+        for(int num : nums)
+        {
+            if(freq[num] == 1 && (num == 0 || freq[num-1] == 0) && freq[num+1] == 0)
+                ans.add(num);
         }
         return ans;
     }
