b2e7ac101b8172fb4c34a5edb98135bd87b76de7
mkshashank <84658486+mkshashank@users.noreply.github.com>
2025-11-27 22:59:39 +0530

    Time: 13 ms (36.21%), Space: 45.6 MB (91.59%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,15 +1,17 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
+        Set<Integer> hs = new HashSet<Integer>();
         int[] arr = new int[2];
         int sum = 0;
         int actualSum = (nums.length * (nums.length + 1)) / 2;
         for(int i = nums.length - 1; i >= 0; i--)
         {
-            if(i > 0 && nums[i] == nums[i-1])
+            if(hs.contains(nums[i]))
             {
                 arr[0] = nums[i];
                 continue;
             }
+            hs.add(nums[i]);
             sum += nums[i];
         }
         arr[1] = actualSum - sum;
